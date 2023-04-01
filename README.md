f312dbb981210bafda3cac7e5e2e22011aec9567
Shadi <Shadialanzi0@gmail.com>
2023-04-01 04:37:05 +0300

    added  files

---
+++ README.md
@@ -0,0 +1,6 @@
+basicalc is a simple npm package that can do 
+1- additon 
+2- subtraction
+3- multiplication
+4- division
+5- powers
---
+++ index.js
@@ -0,0 +1,29 @@
+const addition = (a , c) =>{
+
+    return (a + c)
+}
+
+const  subtraction = (a , c) => {
+    return (a - c)
+}
+
+const multiplication = (a  , c) =>{
+
+    return (a * c)
+}
+
+const division = (a , c) =>{
+    return ( a / c)
+}
+
+const power = (a , c) =>{
+    return (a ** c)
+
+}
+
+
+console.log(addition( 1 , 1))
+console.log(subtraction( 1 , 1))
+console.log(multiplication( 1 , 10))
+console.log(division( 6 , 2))
+console.log(power( 2 , 2))
